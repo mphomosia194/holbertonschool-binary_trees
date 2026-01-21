caa6949a6f6c0f27c81c26ac19649b7ad04f4e7f
mphomosia194 <mphomosia194@gmail.com>
2026-01-21 10:11:13 +0000

    Fix binary_tree_balance to be self-contained

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * height - measures height of a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: height
+ */
+static size_t height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree
  *
@@ -8,14 +30,9 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return ((int)height(tree->left) -
+		(int)height(tree->right));
 }
